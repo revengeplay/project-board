09e00dccf83dbee533a98252153a64d2bd3bf70a
revengeplay <revengeplay@naver.com>
2022-12-05 21:28:39 +0900

    #12 - AuditingFields 추출
    생성일,생성자,수정자,수정일 추출

---
+++ src/test/java/com/project/board/repository/JpaRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.project.board.repository;
+
+import com.project.board.config.JpaConfig;
+import com.project.board.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private ArticleRepository articleRepository;
+    private ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,@Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @Test
+    void name() {
+    }
+}
