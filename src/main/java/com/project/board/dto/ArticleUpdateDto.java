b7aeaf99c114be4b4a33aaac77e7447924fe7c34
revengeplay <revengeplay@naver.com>
2022-12-09 17:33:35 +0900

    #21 service-test

---
+++ src/main/java/com/project/board/dto/ArticleUpdateDto.java
@@ -0,0 +1,14 @@
+package com.project.board.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * A DTO for the {@link com.project.board.domain.Article} entity
+ */
+public record ArticleUpdateDto(String title, String content,
+                               String hashtag) {
+    public static ArticleUpdateDto of( String title, String content, String hashtag) {
+        return new ArticleUpdateDto( title, content, hashtag);
+    }
+}
---
+++ src/main/java/com/project/board/service/ArticleService.java
@@ -0,0 +1,43 @@
+package com.project.board.service;
+
+import com.project.board.domain.type.SearchType;
+import com.project.board.dto.ArticleDto;
+import com.project.board.dto.ArticleUpdateDto;
+import com.project.board.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String searchKeyword) {
+        return Page.empty();
+
+
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticles(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
